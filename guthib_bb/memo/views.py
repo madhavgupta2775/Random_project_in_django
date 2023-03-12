ee6a4bb274e745934b1a59466042719530849eef
madhav <guptamadhav2775@gmail.com>
2023-03-12 14:00:23 +0530

    completed database by changes in models.py, TODO#1 yet to be done

--- guthib_bb/memo/views.py
+++ guthib_bb/memo/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 
 # Create your views here.
 
-posts = [
-    {
-        'title': 'First memo',
-        'content': 'first memo content',
-        'date_posted': 'March 8, 2023',
-        'date_due': 'March 12, 2023'
-    },
-    {
-        'title': 'sec memo',
-        'content': 'sec memo content',
-        'date_posted': 'March 9, 2023',
-        'date_due': 'March 13, 2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'memo/home.html', context)
 
