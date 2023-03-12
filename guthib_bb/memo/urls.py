1f8a550f78b32e1ee5df1b494367d3c06a6fc76d
madhav <guptamadhav2775@gmail.com>
2023-03-12 22:53:04 +0530

    improved the frontend and the redirects

--- guthib_bb/memo/urls.py
+++ guthib_bb/memo/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='memo_home'),
-    path('about/', views.about, name='memo_about'),
+    path('about/', views.about, name='memo_about')
 ]
