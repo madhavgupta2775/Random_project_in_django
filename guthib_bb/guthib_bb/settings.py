ac5a1a6b78037f414ddbfc08479f0353f2d4eab6
madhav <guptamadhav2775@gmail.com>
2023-05-09 23:33:34 +0530

    improved stuff

--- guthib_bb/guthib_bb/settings.py
+++ guthib_bb/guthib_bb/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'memo_home'
+
+LOGIN_URL = 'login'
--- guthib_bb/users/views.py
+++ guthib_bb/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -9,13 +10,16 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created! You are now able to log in.')
             form.save()
-            return redirect('memo_home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 # messages.debug
 # messages.info
