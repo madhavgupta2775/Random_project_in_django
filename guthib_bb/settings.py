069b55c3c3bdca68806e8f78839be75187268a97
madhav <guptamadhav2775@gmail.com>
2023-05-17 14:29:17 +0530

    updated gitignore and removed media files

--- guthib_bb/settings.py
+++ guthib_bb/settings.py
@@ -131,6 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'backend-static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
