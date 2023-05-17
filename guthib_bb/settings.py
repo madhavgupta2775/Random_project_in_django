0ce0f50d894322f0d0d9a8ed242b4c28d628034c
madhav <guptamadhav2775@gmail.com>
2023-05-17 08:12:12 +0530

    updated settings.py

--- guthib_bb/settings.py
+++ guthib_bb/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-quwkdc#t1i$9jg9hteb74-rnj_b@+c2!i7oeh9$ob#spwsx5s!'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "True") == 'True'
 
-ALLOWED_HOSTS = ['random-project-in-django.onrender.com']
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(",")
 # ALLOWED_HOSTS = []
 
 
