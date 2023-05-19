fca0f4acac71e5cdb5b8399c23804b7f9c1823e0
madhav <guptamadhav2775@gmail.com>
2023-05-19 22:26:11 +0530

    fixed error when trying to access memos without being loggin in

--- memo/urls.py
+++ memo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='memo_home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('', login_required(PostListView.as_view()), name='memo_home'),
+    path('post/<int:pk>/', login_required(PostDetailView.as_view()), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
