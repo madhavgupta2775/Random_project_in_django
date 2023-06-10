86614580ed81b3bdf029adae066b0c2e53eac092
madhav <guptamadhav2775@gmail.com>
2023-06-10 22:37:42 +0530

    added account options on profile

---
+++ static/homepage/comments.js
@@ -0,0 +1,19 @@
+function showDropdown(event) {
+    event.stopPropagation(); // Stop event bubbling
+  
+    var dropdown = event.currentTarget.nextElementSibling;
+    dropdown.classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
