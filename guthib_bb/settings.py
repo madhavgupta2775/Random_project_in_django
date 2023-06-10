deeadec6a560175f7115e8cd56be8133b630755c
madhav <guptamadhav2775@gmail.com>
2023-06-10 19:20:13 +0530

    added allauth templates

--- guthib_bb/settings.py
+++ guthib_bb/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'common/templates'),
+            os.path.join(BASE_DIR, 'common/templates/allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
