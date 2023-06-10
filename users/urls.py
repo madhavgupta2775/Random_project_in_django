b19050f9f5b5895bed61e91e5d8867824e39dc28
madhav <guptamadhav2775@gmail.com>
2023-06-11 00:22:51 +0530

    removed useless code

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,6 @@ from .models import User
 urlpatterns = [
     path('<int:pk>/profile_update/', views.profile_update, name='profile_update'),
     path('<int:pk>/profile/', views.profile, name="profile"),
-    path('additional-info/', views.complete_additional_info, name='additional_info'),
     path('<int:pk>/memos/', UserMemosView.as_view(), name='user_memos'),
 ]
 
