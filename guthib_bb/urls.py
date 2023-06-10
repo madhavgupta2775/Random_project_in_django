399e0bb206af1985cbbd0d32ad03239b42d756c0
madhav <guptamadhav2775@gmail.com>
2023-06-10 05:57:01 +0530

    added password reset feature

--- guthib_bb/urls.py
+++ guthib_bb/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('memo/', include('memo.memo_urls')),
     path('announcement/', include('memo.announcement_urls')),
 ]
